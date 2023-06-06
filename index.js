a512e1f5c954a296d47a276e5662851c722db5f8
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-05 22:37:47 -0500

    Color Mode logic

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const gameBoard = document.querySelector('.game-boards')
 const playerCard = document.querySelector('.card-one')
 const botCard = document.querySelector('.card-two')
 const body = document.querySelector('body')
+const player = document.querySelectorAll('.player')
+const footer = document.querySelector('.footer')
+const footerTitle = document.querySelector('.footer-title')
+const githubLogo = document.querySelector('.github-logo')
 const elementArray = [
     colorModeBTN, title,
     gameBoard, playerCard,
-    botCard, header, body ]
+    botCard, header, footer, 
+    footerTitle, githubLogo, body ]
 
 
 colorModeBTN.addEventListener('click', () => {
