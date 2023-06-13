23367dd239795b3be633763d4f0a7c8bf38ecdbf
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-12 21:14:44 -0500

    Modal Logic

--- dom.js
+++ dom.js
@@ -3,20 +3,21 @@ import { Gameboard } from "./index.js"
 const colorModeBTN = document.querySelector('.colorMode')
 const header = document.querySelector('.header')
 const title = document.querySelector('.title')
-const gameBoard = document.querySelector('.game-boards')
-const playerCard = document.querySelector('.card-one')
-const botCard = document.querySelector('.card-two')
+const gameBoardDOM = document.querySelector('.game-boards')
+const playerCardDOM = document.querySelector('.card-one')
+const botCardDOM = document.querySelector('.card-two')
 const body = document.querySelector('body')
 const player = document.querySelectorAll('.player')
 const footer = document.querySelector('.footer')
 const footerTitle = document.querySelector('.footer-title')
 const githubLogo = document.querySelector('.github-logo')
 const playBTN = document.querySelector('.play')
-const squareArray = []
+const playerSquareArray = []
+const botSquareArray = []
 const elementArray = [
     colorModeBTN, title,
-    gameBoard, playerCard,
-    botCard, header, footer, 
+    gameBoardDOM, playerCardDOM,
+    botCardDOM, header, footer, 
     footerTitle, githubLogo, body ]
 
 
@@ -37,7 +38,7 @@ colorModeBTN.addEventListener('click', () => {
     }
 })
 
-function createBoard(cardEl) {
+function createBoard(cardEl, board) {
     function createDiv() {
       const newDiv = document.createElement('div')
       newDiv.setAttribute('class', 'square')
@@ -53,25 +54,97 @@ function createBoard(cardEl) {
             div.style.minHeight = '15px'
             div.style.minWidth = '15px'
             div.style.border = '1px solid black'
-            squareArray.push(div)
-            cardEl.append(div) 
+            if (board == 'PLAYER') playerSquareArray.push(div)
+            if (board == 'BOT') botSquareArray.push(div)
+            cardEl.append(div)
             count++
         }
       }
     generateSquares()
-    const divs = document.querySelectorAll('.square')
+}
+
+function createBoat(cord, boardSide) {
+    const cordDiv = document.createElement('div')
+    cordDiv.setAttribute('class', 'ship')
+    cordDiv.style.height = '100%'
+    if (boardSide == 'PLAYER') {
+        if (playerSquareArray[cord].childNodes.length < 1) {
+            cordDiv.style.backgroundColor = 'blue'
+            playerSquareArray[cord].appendChild(cordDiv)
+        }
+    } else if (boardSide == 'BOT') {
+        if (botSquareArray[cord].childNodes.length < 1) {
+            cordDiv.style.backgroundColor = 'grey'
+            botSquareArray[cord].appendChild(cordDiv)
+        }
+    }
+    
+}
+
+function generateBoat(start, end, ship) {
+    const startLength = `${start}`.length
+    const endLength = `${end}`.length
+    let incrementer;
     let count = 0
-    while (count < divs.length) {
-        const divEL = divs[count]
-        divEL.addEventListener('mouseover', () => {
-            divEL.style.cursor = 'pointer'
-        })
-        count++
+    const startString = start.toString()
+    const startArray = []
+    const endString = end.toString()
+    const endArray = []
+    if (startLength == 1 && endLength == 1) {
+        while (start < end) {
+            createBoat(start, ship)
+            start++
+        }
+    } else if (startLength == 1) {
+        while (count < 2) {
+            endArray.push(endString[count])
+            count++
+        }
+        let startIndex = endArray.indexOf(startString)
+        startIndex == 1 ? startIndex = 0 : null
+        count = 0
+        while (count < endArray[startIndex]) {
+            createBoat(parseInt(`${count}${start}`), ship)
+            count++
+        }
+    } else if (endLength == 1) {
+        while (count < 2) {
+            startArray.push(startString[count])
+            count++
+        }
+        let endIndex = startArray.indexOf(endString)
+        endIndex == 1 ? endIndex = 0 : null 
+        count = 0
+        while (count < startArray[endIndex]) {
+            createBoat(parseInt(`${count}${end}`), ship)
+            count++
+        }
+    } else if (startLength == 2 && endLength == 2) {
+        while (count < 2) {
+            startArray.push(startString[count])
+            endArray.push(endString[count])
+            count++
+        }  
+        const firstDigitCheck = startArray[0] == endArray[0]
+        const secondDigitCheck = startArray[1] == endArray[1]
+        if (firstDigitCheck) {
+            incrementer = startArray[1]
+            while (incrementer < endArray[1]) {
+                createBoat(parseInt(`${startArray[0]}${incrementer}`), ship)
+                incrementer++
+            }
+        } else if (secondDigitCheck) {
+            incrementer = startArray[0]
+            while (incrementer < endArray[0]) {
+                createBoat(parseInt(`${incrementer}${endArray[1]}`), ship)
+                incrementer++
+            }
+        }
     }
 }
 
-function renderShips() {
-    const card = Gameboard()
+function renderShips(board, boardSide) {
+    const card = board
     let index = 1
     while (index < 6) {
         const positionData = card[`ship${index}`]
@@ -82,82 +155,201 @@ function renderShips() {
         const secondSquareSearchStart = positisonCords[1][1]
         let start = parseInt(`${firstSquareSearchStart - 1}${firstSquareSearchEnd - 1}`)
         let end = parseInt(`${secondSquareSearchStart - 1}${secondSquareSearchEnd - 1}`)
-        squareArray[start].style.backgroundColor = 'blue'
-        squareArray[end].style.backgroundColor = 'blue'
-        const startLength = `${start}`.length
-        const endLength = `${end}`.length
-        let count = 0
-        if (startLength == 1 && endLength == 1) {
-            while (start < end) {
-                squareArray[start].style.backgroundColor = 'blue'
-                start++
-            }
-        } else if (startLength == 1) {
-            const endString = end.toString()
-            const endArray = []
-            count = 0 
-            while (count < 2) {
-                endArray.push(endString[count])
-                count++
-            }
-            let startIndex = endArray.indexOf(start.toString())
-            startIndex == 1 ? startIndex = 0 : null 
-            count = 0
-            while (count < endArray[startIndex]) {
-                squareArray[parseInt(`${count}${start}`)].style.backgroundColor = 'blue'
-                count++
-            }
-        } else if (endLength == 1) {
-            const startString = start.toString()
-            const startArray = []
-            count = 0 
-            while (count < 2) {
-                startArray.push(startString[count])
-                count++
-            }
-            let endIndex = startArray.indexOf(end.toString())
-            endIndex == 1 ? endIndex = 0 : null 
-            count = 0
-            while (count < startArray[endIndex]) {
-                squareArray[parseInt(`${count}${end}`)].style.backgroundColor = 'blue'
-                count++
-            }
-        } else if (startLength == 2 && endLength == 2) {
-            const startString = start.toString()
-            const endString = end.toString()
-            const startArray = []
-            const endArray = []
-            count = 0
-            while (count < 2) {
-                startArray.push(startString[count])
-                endArray.push(endString[count])
-                count++
-            }
-            const firstDigitCheck = startArray[0] == endArray[0]
-            const secondDigitCheck = startArray[1] == endArray[1]
-            if (firstDigitCheck) {
-                let incrementer = startArray[1]
-                while (incrementer < endArray[1]) {
-                    squareArray[parseInt(`${startArray[0]}${incrementer}`)].style.backgroundColor = 'blue'
-                    incrementer++
-                }
-            } else if (secondDigitCheck) {
-                let incrementer = startArray[0]
-                while (incrementer < endArray[0]) {
-                    squareArray[parseInt(`${incrementer}${endArray[1]}`)].style.backgroundColor = 'blue'
-                    incrementer++
-                }
-            }
-        }
+        generateBoat(start, end, boardSide)
+        createBoat(end, boardSide)
         index++
     }
     return card
 }
-createBoard(playerCard)
-const playerBoard = renderShips()
+
+function getShipLength(ship) {
+    let shipLength;
+    if (ship == 1) {
+        shipLength = 1
+    } else if (ship == 2 || ship == 3) {
+        shipLength = 2
+    } else if (ship == 4) {
+        shipLength = 3
+    } else if (ship == 5) {
+        shipLength = 4
+    }
+    return shipLength
+}
+
+const playerBoard = Gameboard()
+const modalEl = document.querySelector('.modal')
+const bgOpacity = document.querySelector('.backColor')
+const nextBTN = document.querySelector('.nextBTN')
+const inputEls = document.querySelectorAll('input')
+let selectedShip;
+let selectedInput;
+
+inputEls.forEach(input => {
+    input.addEventListener('input', () => {
+        const shipIndex = Array.from(inputEls).indexOf(input) + 1
+        const curShip = playerBoard[`ship${shipIndex}`]
+        selectedInput = shipIndex
+        selectedShip = curShip
+        const value = input.value
+        let valid = true
+        function checkNum(value) {
+            const alpha = [
+            'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z']
+            if (value > 10 || value < 1) {
+                input.value = ''
+                input.placeholder = 'Number must be 1-10'
+                input.style.border = '2px solid red'
+                valid = false
+            } else {
+                valid = true
+                input.style.border = '2px solid lightGreen'
+            }
+            alpha.forEach(letter => {
+                if (value.includes(letter)) {
+                    input.value = ''
+                    input.placeholder = 'Must enter number'
+                    input.style.border = '2px solid red'
+                    valid = false
+                }
+            })
+            if (value.length == 0) {
+                valid = true
+            }
+        }
+        checkNum(value)
+        // Single Digit UX Control
+        if (value.length == 1 && value > 1) {
+            input.value += ','
+        }
+        // Second Digit UX Control
+        if (value.includes(',') && value.length - 1 > value.indexOf(',')) {
+            const secondNumberIndex = value.indexOf(',') + 1
+            const secondNumber = value.slice(secondNumberIndex)
+            checkNum(secondNumber)
+        }
+
+        function checkCords(cords) {
+            let newSet = cords
+            cords.forEach(cord => {
+                cord.forEach(num => {
+                    if (num > 10) {
+                        const curArrNumIndex = cord.indexOf(num)
+                        const subtract = cords[1][curArrNumIndex] - cords[0][curArrNumIndex]
+                        newSet[1][curArrNumIndex] -= subtract * 2
+                        const firstArrayCache = newSet[0]
+                        const secondArrayCache = newSet[1]
+                        newSet = [secondArrayCache, firstArrayCache]
+                    }
+                })
+            })
+            return newSet
+        }
+
+        function createCords(ship, cord) {
+            const shipLength = getShipLength(ship)
+            const firstCordNum1 = cord.slice(0, cord.indexOf(','))
+            const firstCordNum2 = cord.slice(cord.indexOf(',') + 1)
+            let newCords = [ 
+                [parseInt(firstCordNum1), parseInt(firstCordNum2)],
+                [parseInt(firstCordNum1) + shipLength, parseInt(firstCordNum2)]
+            ]
+            return checkCords(newCords)
+        }
+        //TODO FIX Ghost Render
+        function ghostRender(ship, cord) {
+            if (cord.includes(',')) {
+                document.querySelectorAll('.ship').forEach(ship => {
+                    ship.remove()
+                })
+                const cords = createCords(ship,cord)
+                console.log(playerBoard.move(`ship${shipIndex}`, cords), 'move status')
+                renderShips(playerBoard, 'PLAYER')
+                console.log(playerBoard);
+            }
+            //TODO ^- Render a ghost ship
+        } 
+        if (valid) ghostRender(shipIndex, value)
+    })
+    const rotateBtn = document.querySelector('.rotateBTN');
+    rotateBtn.addEventListener('click', (e) => {
+        e.stopImmediatePropagation();
+        const shipLevel = selectedShip.startCord[1] === selectedShip.endCord[1];
+        const shipColumn = selectedShip.startCord[0] === selectedShip.endCord[0];
+        if (shipLevel) {
+            let endCord = selectedShip.endCord;
+            const shipLength = Math.abs(selectedShip.endCord[0] - selectedShip.startCord[0]);
+            endCord = [selectedShip.startCord[0], selectedShip.startCord[1] + shipLength];
+            selectedShip.endCord = endCord;
+            if (selectedShip.endCord[1] < selectedShip.startCord[1]) {
+                const cache = selectedShip.startCord;
+                selectedShip.startCord = selectedShip.endCord;
+                selectedShip.startCord[1] += 1;
+                selectedShip.endCord = cache;
+            }
+            document.querySelectorAll('.ship').forEach(ship => {
+                ship.remove();
+            });
+            playerBoard.move(`ship${selectedInput}`, [selectedShip.startCord, selectedShip.endCord]);
+            console.log(selectedShip, 'level');
+            renderShips(playerBoard, 'PLAYER');
+        } else if (shipColumn) {
+            let endCord = selectedShip.endCord;
+            const shipLength = Math.abs(selectedShip.endCord[1] - selectedShip.startCord[1]);
+            endCord = [selectedShip.startCord[0] + shipLength, selectedShip.startCord[1]];
+            selectedShip.endCord = endCord;
+            if (selectedShip.endCord[0] < selectedShip.startCord[0]) {
+                const cache = selectedShip.startCord;
+                selectedShip.startCord = selectedShip.endCord;
+                selectedShip.startCord[0] += 1;
+                selectedShip.endCord = cache;
+            }
+            document.querySelectorAll('.ship').forEach(ship => {
+                ship.remove();
+            });
+            console.log(selectedShip, 'column');
+            renderShips(playerBoard, 'PLAYER');
+        }
+    });
+})
+
+
+//TODO ADD SUBMIT LOGIC -> just alert errors
+//TODO DELETE UNNECESSARY LOGIC AFTER THINKING ABOUT IT
+nextBTN.addEventListener('click', () => {
+    const inputArray = []
+    let count = 1
+    while (count < 6) {
+        const playerStart = playerBoard[`ship${count}`].startCord
+        const playerEnd = playerBoard[`ship${count}`].endCord
+        const input = document.querySelector(`.cordInput${count}`)
+        inputArray.push(input.value)
+        count++
+    }
+    console.log(inputArray)
+    modalEl.childNodes.forEach(node => {
+        modalEl.remove(node)
+    })
+    playBTN.classList.remove('deactive')
+    bgOpacity.classList.remove('active')
+    modalEl.classList.remove('active')
+    bgOpacity.classList.add('deactive')
+    modalEl.classList.add('deactive')
+    renderShips(playerBoard, 'PLAYER')
+})
+createBoard(playerCardDOM, 'PLAYER')
 //TODO allow them to be dragged 
 playBTN.addEventListener('click', () => {
     playBTN.remove()
-    createBoard(botCard)
-    const botBoard = renderShips()
+    createBoard(botCardDOM, 'BOT')
+    const botBoard = Gameboard()
+    renderShips(botBoard, 'BOT')
 })
